c312314ddb7c2dca2e3a7accc42744a15efbf7c2
Your Name <email@example.com>
2020-09-01 13:11:23 -0400

    no8

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-the form again."; } else{ $from="From: $name<$email>\r\nReturn-path: $email"; $subject="Message sent using your contact form"; mail("youremail@yoursite.com", $subject, $message, $from); echo "Email sent!"; } } ?>
-
+/*the form again."; } else{ $from="From: $name<$email>\r\nReturn-path: $email"; $subject="Message sent using your contact form"; mail("youremail@yoursite.com", $subject, $message, $from); echo "Email sent!"; } } ?>
+*/
 
 document.querySelector('.menu_left').innerHTML = `
     <ul class="menu_left" id="menu">
